b62fb8e2cde9209585d7642fbc892fb5619a858f
mutazabutaha <mutazabutaha13@outlook.com>
2023-07-29 21:12:16 +0000

    lab32

---
+++ README.md
@@ -0,0 +1,12 @@
+## LAB - Class 32
+
+## Project: drf-api-permissions-postgres
+
+## Author: Almoutaz Abu-taha
+* Note : Ahmad Falah helped me 
+
+## How to run tests?
+* docker-compose run web python manage.py test
+
+## How to run your application:
+* python manage.py runserver
---
+++ Snacks/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Snack
+from rest_framework.generics import ListAPIView, RetrieveAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import SnackSerializer
+from .permissions import Auth_user
+# Create your views here.
+
+
+class SnackListView(ListCreateAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+
+
+class SnackDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+    permission_classes = [Auth_user]
